98c1859055c36baadde5e63e0e6bae75222783e0
Dlicy <146825414+Dlicy@users.noreply.github.com>
2024-03-09 17:47:33 +0800

    blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://Dlicy.github.io',	
+	site: 'https://astro-blog.pages.github.io',	
 	integrations: [mdx(), sitemap()],
 });
